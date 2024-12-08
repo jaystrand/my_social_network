b72ad4d01aa86ebad5749b68c8e21b79bab213aa
Hydro <jermaine.strand@yahoo.com>
2024-12-08 17:00:11 -0500

    changes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,20 +1,10 @@
 import { Router } from 'express';
-<<<<<<< HEAD
-import { thoughtsRouter } from './thoughtsRoutes.ts';
-import { UsersRouter } from './usersRoutes.ts';
+import { thoughtsRouter } from './thoughtRoutes.ts';
+import { usersRouter } from './userRoutes.ts';
 
 const router = Router();
 
 router.use('/thoughts', thoughtsRouter);
-router.use('/users', UsersRouter);
-=======
-import { thoughtRouter } from './thoughtRoutes.ts';
-import { UserRouter } from './userRoutes.ts';
-
-const router = Router();
-
-router.use('/thought', thoughtRouter);
-router.use('/user', UserRouter);
->>>>>>> main
+router.use('/users', usersRouter);
 
 export default router;
